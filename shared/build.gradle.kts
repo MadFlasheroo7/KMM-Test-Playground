de1ebfc0eaa2bd8444805a228ccda75f8485b7d2
sethJayesh <jayesh@1pharmacy.io>
2025-04-24 12:16:46 +0530

    setup ios cmp running

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -15,9 +15,16 @@ kotlin {
         }
     }
 
-    iosX64()
-    iosArm64()
-    iosSimulatorArm64()
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "Shared"
+            isStatic = true
+        }
+    }
 
     jvm()
 
