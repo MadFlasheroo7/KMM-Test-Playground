ea54b0ce3aab5d3fd711e36aa2799a40dd740e1b
Madflasheroo7 <jayeshseth45@gmail.com>
2025-05-18 02:01:00 +0530

    add extensions

--- build-logic/convention/src/main/kotlin/pro/jayeshseth/kmmPlayground/ProjectExtension.kt
+++ build-logic/convention/src/main/kotlin/pro/jayeshseth/kmmPlayground/ProjectExtension.kt
@@ -1,9 +1,27 @@
 package pro.jayeshseth.kmmPlayground
 
 import org.gradle.api.Project
+import org.gradle.api.artifacts.Dependency
 import org.gradle.api.artifacts.VersionCatalog
 import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.api.artifacts.dsl.DependencyHandler
+import org.gradle.api.provider.Provider
 import org.gradle.kotlin.dsl.getByType
 
 val Project.libs
     get(): VersionCatalog = extensions.getByType<VersionCatalogsExtension>().named("libs")
+
+fun DependencyHandler.commonMain(dependencyNotation: Any): Dependency? =
+    add("commonMainImplementation", dependencyNotation)
+
+fun DependencyHandler.androidMain(dependencyNotation: Any): Dependency? =
+    add("androidMainImplementation", dependencyNotation)
+
+fun DependencyHandler.desktopMain(dependencyNotation: Any): Dependency? =
+    add("desktopMainImplementation", dependencyNotation)
+
+fun DependencyHandler.implementation(dependencyNotation: Any): Dependency? =
+    add("implementation", dependencyNotation)
+
+fun DependencyHandler.debugImplementation(dependencyNotation: Any): Dependency? =
+    add("debugImplementation", dependencyNotation)
